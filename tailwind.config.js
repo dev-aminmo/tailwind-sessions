aba165dc7d66ff9e59eb1f8fec4fa9a303b5a3c4
MezianeKhalil <khalilmez2000@gmail.com>
2022-04-01 23:28:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./views/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins":['Poppins', 'sans-serif']
+      },
+      flex: {
+        '2': '2 2 0%'
+      }
+    },
+  },
+  plugins: [],
+}
